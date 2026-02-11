28e450809673506886f99165311d284564e57351
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-02-11 13:06:21 +0530

    image view screen added to the blog page

--- src/assets/Components/core/Blog/blog_ui/ImageUi.jsx
+++ src/assets/Components/core/Blog/blog_ui/ImageUi.jsx
@@ -1,9 +1,26 @@
-import { motion } from "framer-motion";
-import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect } from "react";
 
 const ImageUi = ({ data }) => {
-  const imageUrl = data?.imageUrl;
+  // const imageUrl = data?.imageUrl;
+  const imageUrl =
+    "https://res.cloudinary.com/dwpplwqzs/image/upload/v1770131913/Portfolio-site/blogs/thumbnails/xjtsnvkebqz7ikmlqggg.jpg";
   const [showTitle, setShowTitle] = useState(false);
+  const [viewImage, setViewImage] = useState(null);
+
+  const imageViewHandler = (url) => setViewImage(url);
+
+  useEffect(() => {
+    if (viewImage) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [viewImage]);
 
   return (
     <motion.figure
@@ -16,7 +33,7 @@ const ImageUi = ({ data }) => {
       "
       onHoverStart={() => setShowTitle(true)}
       onHoverEnd={() => setShowTitle(false)}
-      onTap={() => setShowTitle((prev) => !prev)} // 📱 mobile tap
+      onTap={() => setShowTitle((prev) => !prev)} //  mobile tap
     >
       {/* IMAGE / FALLBACK */}
       {imageUrl ? (
@@ -27,6 +44,7 @@ const ImageUi = ({ data }) => {
           initial={{ scale: 1.05 }}
           animate={{ scale: 1 }}
           transition={{ duration: 0.4 }}
+          onClick={() => imageViewHandler(imageUrl)}
         />
       ) : (
         <div className="flex flex-col items-center justify-center py-16 text-gray-400">
@@ -54,6 +72,41 @@ const ImageUi = ({ data }) => {
           {data.title}
         </motion.figcaption>
       )}
+
+      <AnimatePresence>
+        {viewImage && (
+          <motion.div
+            className="fixed inset-0 bg-black/90 flex items-center justify-center z-50 cursor-zoom-out"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{
+              duration: 0.2,
+              type: "spring",
+              stiffness: 200,
+              damping: 15,
+            }}
+            onClick={() => setViewImage(null)}
+          >
+            <motion.img
+              initial={{ scale: 0.8, y: 20 }}
+              animate={{ scale: 1, y: 0 }}
+              exit={{ scale: 0.8, opacity: 0 }}
+              whileHover={{ border: "1px solid #fff" }}
+              whileTap={{ border: "1px solid #fff" }}
+              transition={{
+                type: "spring",
+                stiffness: 120,
+                damping: 18,
+              }}
+              src={viewImage}
+              alt="Full View"
+              className="max-h-[90vh] max-w-[90vw] object-contain rounded-lg"
+              onClick={(e) => e.stopPropagation()}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </motion.figure>
   );
 };
